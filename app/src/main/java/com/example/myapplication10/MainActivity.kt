56ad99848f7b864eef7db49bbcbef03aca60c263
秋山智貴 <akiyama@rightcode.co.jp>
2019-04-02 11:16:45 +0900

    テスト３

--- app/src/main/java/com/example/myapplication10/MainActivity.kt
+++ app/src/main/java/com/example/myapplication10/MainActivity.kt
@@ -25,6 +25,8 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        Log.d("TEST_TEST", "ddd")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
